dc73064fc018e32bec2925f4f909e19bd7bb8b04
Nicolas <nicolas.sabbatini.95@gmail.com>
2018-05-07 19:27:21 -0300

    Modularity

--- src/game/main.lua
+++ src/game/main.lua
@@ -1,8 +1,9 @@
 local game = {}
 
 local key = require 'libs/simpleKey'
+local comets = require('game/comets')
 
-local HighScore, score, b, font,  backgraund, star, planet, comets
+local HighScore, score, b, font,  backgraund, star, planet
 
 local function saveHighScore()
     love.filesystem.write('score', 'return ' .. data)
@@ -61,57 +62,6 @@ function game:load()
     planet.x = star.x + planet.d
     planet.y = star.y + planet.d
     
-    comets = {}
-    comets.c = {}
-    comets.max = 3
-    comets.time = 0
-    function comets:restart()
-        comets.c = {}
-        comets.max = 3
-        comets.time = 0
-    end
-    function comets:new()
-        local c = {}
-        local angle = math.deg(math.random(0, 360))
-        c.sin =math.sin(angle)
-        c.cos =math.cos(angle)
-        c.d = 400
-        c.x = star.x + c.sin * c.d
-        c.y = star.y + c.cos * c.d        
-        c.v = math.random(80, 150)
-        table.insert(comets.c, c)
-        print(#comets.c)
-    end
-    function comets:update(dt)
-        comets.time = comets.time + dt
-        if comets.time > comets.max then
-            comets:new()
-            if comets.max > 0.3 then
-                comets.max = comets.max - (0.3/comets.max)
-                print(comets.max)
-            end
-            comets.time = 0
-        end
-        for k, c in pairs(comets.c) do
-            local pos = math.sqrt(((c.x-planet.x)^2 + (c.y-planet.y)^2))
-            if pos < 17 then
-                print('comet', pos)
-                gamefriz()
-            end
-            c.d = c.d - c.v*dt
-            c.x = star.x + c.sin * c.d
-            c.y = star.y + c.cos * c.d
-            -- If the element is remove the nex element is going to take
-            -- its place, k-1 avoid skiping an element
-            if c.d < 10 then table.remove(comets.c, k); k = k-1 end
-            print(#comets.c)
-        end
-    end
-    function comets:draw()
-        for _, c in pairs(comets.c) do
-            love.graphics.circle('fill', c.x, c.y, 10)
-        end
-    end
 
 end
 
@@ -121,7 +71,7 @@ function game:update(dt)
     -- Star
     star.ps:update(dt)
     -- Comets
-    comets:update(dt)
+    comets:update(dt, star, planet)
 
     -- Player
     if key:checkDown('space') then planet.d = planet.d + (150 * dt)
