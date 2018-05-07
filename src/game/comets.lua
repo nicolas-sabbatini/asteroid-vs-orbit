d5f5613e923b610f5c5a31c434f92cf4343a9058
Nicolas <nicolas.sabbatini.95@gmail.com>
2018-05-07 20:29:59 -0300

    improve math

--- src/game/comets.lua
+++ src/game/comets.lua
@@ -12,15 +12,14 @@ end
 
 function comets:new(star)
     local c = {}
-    local angle = math.deg(math.random(0, 360))
+    local angle = math.deg(love.math.random(0, 360))
     c.sin =math.sin(angle)
     c.cos =math.cos(angle)
     c.d = 400
     c.x = star.x + c.sin * c.d
     c.y = star.y + c.cos * c.d        
-    c.v = math.random(80, 150)
+    c.v = love.math.random(80, 150)
     table.insert(comets.c, c)
-    print(#comets.c)
 end
 
 function comets:update(dt, star, planet)
@@ -28,16 +27,14 @@ function comets:update(dt, star, planet)
     if comets.time > comets.max then
         comets:new(star)
         if comets.max > 0.3 then
-            comets.max = comets.max - (0.3/comets.max)
-            print(comets.max)
+            comets.max = comets.max - (comets.max/30)
         end
         comets.time = 0
     end
     for k, c in pairs(comets.c) do
         local pos = math.sqrt(((c.x-planet.x)^2 + (c.y-planet.y)^2))
         if pos < 17 then
-            print('comet', pos)
-            gamefriz()
+            planet.alive = false
         end
         c.d = c.d - c.v*dt
         c.x = star.x + c.sin * c.d
@@ -45,7 +42,6 @@ function comets:update(dt, star, planet)
         -- If the element is remove the nex element is going to take
         -- its place, k-1 avoid skiping an element
         if c.d < 10 then table.remove(comets.c, k); k = k-1 end
-        print(#comets.c)
     end
 end
 
